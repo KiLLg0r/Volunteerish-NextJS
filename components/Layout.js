a539c78df50dcbb07d8d3f7e38617ad3a82d42b9
Robert Oblesniuc <rob.oblesniuc@gmail.com>
2022-06-01 16:14:25 +0300

    Changed from scss to css

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import styles from "./Layout.module.scss";
+import styles from "./Layout.module.css";
 
 export const Layout = ({ children }) => {
   return <div className={styles.container}>{children}</div>;
